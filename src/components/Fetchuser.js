e809e8c7473f161c715052de80d79408e79ed34c
j-sylvia <150928883+j-sylvia@users.noreply.github.com>
2024-03-24 13:42:49 +0530

    Update Fetchuser.js

--- src/components/Fetchuser.js
+++ src/components/Fetchuser.js
@@ -38,11 +38,11 @@ function Fetchuser() {
 
   return (
     <div className='container'>
-      <Row xs={1} sm={2} md={3} lg={4} className="g-4 row d-flex justify-content-center align-items-center">
+      <Row xs={1} sm={2} md={3} lg={4} className="g-4">
         <input type="text" className='form-control' placeholder='Search here...' onChange={Filter} />
         {filteruser.map((user) => (
      
-          <Col key={user.id}>
+          <Col key={user.id} className="row d-flex justify-content-center align-items-center">
             <Card style={{ border: '2px solid black', width: '18rem', boxShadow: '2px 10px 15px grey', backgroundColor: 'lightblue' }}>
               <Card.Img style={{ height: '200px' }} variant="top" src={user.image} />
               <Card.Body>
